a2c58b2a9213bbe1c6ac9cfcf59045fef4e50b4f
gustavmr <gustavmr@gmail.com>
2022-03-27 11:04:17 -0500

    Create customized board size on reset

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-function createBlockLine () {
+function createBlockLine (dim) {
     let blockLine = document.createElement("div");
     blockLine.classList.add("blockLine");
     container.appendChild(blockLine);
 
-    for (i = 1; i <= 4; i++) {
+    for (i = 1; i <= dim; i++) {
         let block = document.createElement("div");
         block.classList.add("block");
-        block.textContent = "testing block";
         blockLine.appendChild(block);
     }
-
 }
 
-function createGrid () {
-    for (x = 1; x <= 4; x++) {
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createGrid (dim) {
+    for (x = 1; x <= dim; x++) {
         console.log(x);
-        createBlockLine();
+        createBlockLine(dim);
     }
 }
 
@@ -24,18 +28,22 @@ function highlightBlock (e) {
 }
 
 function resetGrid() {
-    allBlocks.forEach(block => block.classList.remove("hover"));  
+    allBlocks.forEach(block => block.classList.remove("hover")); 
+    let dimension = Math.min(window.prompt("please select # of blocks per side",10),100);
+    console.log(dimension); 
+    removeAllChildNodes(container);
+    createGrid(dimension);
+    let allNewBlocks = document.querySelectorAll(".block");
+    allNewBlocks.forEach(block => block.addEventListener("mouseover", highlightBlock));
 }
 
 const container = document.querySelector("#container");
 const reset = document.querySelector("#reset");
-createGrid();
+createGrid(10);
 const allBlocks = document.querySelectorAll(".block");
-
-
-
 
 allBlocks.forEach(block => block.addEventListener("mouseover", highlightBlock));
 reset.addEventListener("click",resetGrid);
+
 
 
